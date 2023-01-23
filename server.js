7dd6ff82069adab49461f3f0c6e70e88c1fcf25b
aaronleekers <leekaaron5@gmail.com>
2023-01-22 17:24:18 -0800

    Update server.js

--- server.js
+++ server.js
@@ -149,7 +149,8 @@ async function api_search(queryString, callback) {
         Follow this workflow:
         1. Replace the variables in this link with the variables that were passed in.
         2. All variables passed in this link should be, stockName, fromDate, toDate, period. 
-        Link: https://www.eodhistoricaldata.com/api/eod/stockName.US?api_token=63a2477acc2587.58203009&fmt=json&from=fromDate&to=toDate&period=periodTime
+        3. Respond with full link only. Don't add anything else to preface it, as it will be used entirely as the API link.
+        https://www.eodhistoricaldata.com/api/eod/stockName.US?api_token=63a2477acc2587.58203009&fmt=json&from=fromDate&to=toDate&period=periodTime
         Variables: ${extractedInfo}`,
         max_tokens: 3000,
         temperature: .5,
