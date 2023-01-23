06ab2a16c500d896f239c6aa480364ae17146b07
aaronleekers <leekaaron5@gmail.com>
2023-01-23 03:01:41 -0800

    Update server.js

--- server.js
+++ server.js
@@ -206,7 +206,7 @@ async function api_search(queryString) {
         
         Data: ${apiCallDataString}
         `,
-        max_tokens: 4000,
+        max_tokens: 2048,
         temperature: .5,
         stop: "/n",
     })
